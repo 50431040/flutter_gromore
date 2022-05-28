ed88155793c77852dc42db7f3cbad7ae61c88fad
jlq-mac <50431040@qq.com>
2022-05-28 10:26:25 +0800

    feat: 参数校验、更新文档

--- android/src/main/kotlin/net/niuxiaoer/flutter_gromore/PluginDelegate.kt
+++ android/src/main/kotlin/net/niuxiaoer/flutter_gromore/PluginDelegate.kt
@@ -16,7 +16,6 @@ import net.niuxiaoer.flutter_gromore.view.FlutterGromoreSplash
 
 class PluginDelegate(private val activity: Activity, private val binaryMessenger: BinaryMessenger): MethodChannel.MethodCallHandler {
     private val TAG: String = this::class.java.simpleName
-    private var eventSink: EventChannel.EventSink? = null
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         val method: String = call.method
@@ -27,7 +26,6 @@ class PluginDelegate(private val activity: Activity, private val binaryMessenger
         when(method) {
             // 同时请求：READ_PHONE_STATE, COARSE_LOCATION, FINE_LOCATION, WRITE_EXTERNAL_STORAGE权限
             "requestPermissionIfNecessary" -> {
-                Log.d(TAG, "requestPermissionIfNecessary")
                 GMMediationAdSdk.requestPermissionIfNecessary(activity)
                 result.success(true)
             }
@@ -52,10 +50,14 @@ class PluginDelegate(private val activity: Activity, private val binaryMessenger
 
     // 初始化SDK
     private fun initSDK(arguments: Map<String, Any?>?) {
+        require(arguments != null)
+
         // 取出传递的参数
-        val appId = arguments?.get("appId") as? String ?: ""
-        val appName = arguments?.get("appName") as? String ?: ""
-        val debug = arguments?.get("debug") as? Boolean ?: BuildConfig.DEBUG
+        val appId = arguments["appId"] as? String ?: ""
+        val appName = arguments["appName"] as? String ?: ""
+        val debug = arguments["debug"] as? Boolean ?: BuildConfig.DEBUG
+
+        require(appId.isNotEmpty())
 
         val config = GMAdConfig.Builder()
                 .setAppId(appId)
@@ -69,23 +71,24 @@ class PluginDelegate(private val activity: Activity, private val binaryMessenger
     // 开屏广告
     private fun showSplash(arguments: Map<String, Any?>?) {
 
+        require(arguments != null)
+
         val intent = Intent(activity, FlutterGromoreSplash::class.java).apply {
-            putExtra("id", arguments?.get("id") as? String)
-            putExtra("adUnitId", arguments?.get("adUnitId") as? String)
-            putExtra("logo", arguments?.get("logo") as? String)
-            putExtra("muted", arguments?.get("muted") as? Boolean)
-            putExtra("preload", arguments?.get("preload") as? Boolean)
-            putExtra("volume", arguments?.get("volume") as? Float)
-            putExtra("timeout", arguments?.get("timeout") as? Int)
-            putExtra("buttonType", arguments?.get("buttonType") as? Int)
-            putExtra("downloadType", arguments?.get("downloadType") as? Int)
+            putExtra("id", arguments["id"] as? String)
+            putExtra("adUnitId", arguments["adUnitId"] as? String)
+            putExtra("logo", arguments["logo"] as? String)
+            putExtra("muted", arguments["muted"] as? Boolean)
+            putExtra("preload", arguments["preload"] as? Boolean)
+            putExtra("volume", arguments["volume"] as? Float)
+            putExtra("timeout", arguments["timeout"] as? Int)
+            putExtra("buttonType", arguments["buttonType"] as? Int)
+            putExtra("downloadType", arguments["downloadType"] as? Int)
         }
 
-        activity.startActivity(intent)
-        activity.overridePendingTransition(0, 0)
-    }
-
-    fun sendEvent() {
+        activity.apply {
+            startActivity(intent)
+            activity.overridePendingTransition(0, 0)
+        }
 
     }
 
